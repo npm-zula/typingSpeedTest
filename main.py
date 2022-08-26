bbb5ab1131975a1c08a0b5a6bc2a6d1f84da3709
shershah <i200523@nu.edu.pk>
2022-08-26 17:19:12 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import curses
+from curses import wrapper
+import time 
+import random
+
+def startScrn(stdscr):
+    stdscr.clear()
+    stdscr.addstr(1,56,"Welcome to the Typing Speed test!")
+    stdscr.addstr(2,60,"Press any key to continue")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def load_text():
+    with open('text.txt',"r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+    
+
+
+def wpmTest(stdscr):
+    target_text = load_text()
+    curr_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
+    
+   
+    while True:
+        timeElasped = max(time.time() - start_time, 1)
+
+
+        wpm = len(curr_text) / timeElasped
+        wpm = round((wpm * 60)/5)
+
+
+        stdscr.clear()
+        display_text(stdscr, target_text, curr_text,wpm)
+        stdscr.refresh()
+        
+        if "".join(curr_text) == target_text:
+            stdscr.nodelay(False)
+            break
+
+
+
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+
+        if ord(key) == 27:
+            break
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(curr_text) > 0:
+                curr_text.pop()
+        elif len(curr_text) < len(target_text):
+             curr_text.append(key)
+    
+    stdscr.addstr(2,0, "You completed the test! Press any key to continue!")
+    stdscr.getkey()
+
+
+        
+
+def display_text(stdscr, target_text, curr_text, wpm):
+    stdscr.addstr(target_text)
+    stdscr.addstr(1,0, f"WPM: {wpm}")
+
+    for i, char in enumerate(curr_text):
+        correct = target_text[i]
+        color = curses.color_pair(3)
+
+        if(char != correct):
+            color = curses.color_pair(2)
+
+        stdscr.addstr(0,i,char, color)
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_WHITE , curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW , curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_CYAN , curses.COLOR_BLACK)
+    
+    while(True):
+        startScrn(stdscr)
+        wpmTest(stdscr)
+        
+
+wrapper(main)
